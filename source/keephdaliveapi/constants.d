bfa18ef1d35ff886965a2a2e8e4916cbb734250b
Paul Crane <paul@acheronsoft.com>
2018-08-14 23:20:25 -0500

    Add module constants.

---
+++ source/keephdaliveapi/constants.d
@@ -0,0 +1,8 @@
+module keephdaliveapi.constants;
+
+immutable string COMPANY_NAME = "Raijinsoft";
+immutable string PROGRAM_NAME = "keephdalive";
+immutable string WRITE_TO_LOCATIONS_FILENAME = "locations.dat";
+immutable string DEFAULT_LOCATIONS_DATA = "./\n";
+immutable string DEFAULT_WRITE_TO_FILENAME = "keephdalive.txt"; // TODO: Perhaps make it hidden.
+immutable size_t DEFAULT_FILE_WRITE_DELAY = 5;
