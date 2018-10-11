8d805970688106e997e5bbc33df031b1d0763597
Paul Crane <paul@acheronsoft.com>
2018-10-10 22:33:14 -0500

    Add initial code for handling settings.

--- source/keephdaliveapi/settings.d
+++ source/keephdaliveapi/settings.d
@@ -1,6 +1,30 @@
-module settings;
+module keephdaliveapi.settings;
+
+import ctoptions;
+import dpathutils;
+import keephdaliveapi.constants;
+
+private struct Options
+{
+	size_t delay = DEFAULT_FILE_WRITE_DELAY;
+	string filename = DEFAULT_WRITE_TO_FILENAME;
+	string path;
+}
 
 class KeepAliveSettings
 {
-// TODO: Mabye use a lock file?
+	// TODO: Mabye use a lock file?
+	this()
+	{
+		path_ = ConfigPath(COMPANY_NAME, PROGRAM_NAME);
+	}
+
+private:
+	StructOptions!Options settings_;
+	ConfigPath path_;
+}
+
+unittest
+{
+	KeepAliveSettings settings = new KeepAliveSettings;
 }
